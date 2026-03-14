44fde13405e17ca5e05c52ff8e9660235a345504
maquinoted <mario@todoesdigital.com>
2026-03-14 00:35:07 -0600

    FIX: Permitir envíos POST desactivando checkOrigin para el Login

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,24 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite'; 
-import vercel from '@astrojs/vercel/serverless'; // Cambiado a serverless para mayor estabilidad con Postgres
+import vercel from '@astrojs/vercel/serverless';
 
+// https://astro.build/config
 export default defineConfig({
-  // 'server' fuerza a que TODAS las páginas sean dinámicas a menos que digas lo contrario
+  // 'server' fuerza a que TODAS las páginas sean dinámicas
   output: 'server', 
   
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
-    // Esto asegura que las funciones de Vercel tengan acceso a la base de datos de Postgres
+    // Asegura acceso estable a Postgres
     functionPerRoute: false, 
   }),
+
+  // AGREGAMOS ESTO PARA SOLUCIONAR EL ERROR DEL LOGIN
+  security: {
+    checkOrigin: false,
+  },
 
   vite: {
     plugins: [tailwindcss()],
